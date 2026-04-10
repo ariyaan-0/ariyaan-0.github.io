4facb07f729b0c9630282a27a6af46abae85d465
Md Rezaul Hossain <str.rezaul@gmail.com>
2026-04-11 01:11:56 +0600

    update project

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -34,16 +34,16 @@ const CONFIG = {
       },
     },
     external: {
-      header: 'My Products',
+      header: 'Deployed Projects',
       // To hide the `External Projects` section, keep it empty.
       projects: [
         // {
-        //   title: 'Project Name',
+        //   title: 'Hive',
         //   description:
-        //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+        //     'Hive is a hyperlocal social media platform designed to foster real-time community engagement within a 2-3km radius. Built with React 19 and FastAPI, it allows users to share text and media-rich posts, participate in threaded discussions, and influence content visibility through a custom voting system.',
         //   imageUrl:
         //     'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-        //   link: 'https://example.com',
+        //   link: 'https://hiveroom.online',
         // },
         // {
         //   title: 'Project Name',
