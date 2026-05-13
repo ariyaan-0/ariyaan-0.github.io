1f63c16e5082f780fff93d1e54089a5afd1a8e8f
Md Rezaul Hossain <str.rezaul@gmail.com>
2026-05-13 23:11:25 +0600

    update resume link

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -81,7 +81,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://raw.githubusercontent.com/ariyaan-0/ariyaan-0.github.io/98c97fc1298eaa2b138ba94c5bea05c6a19ec913/public/resume_rezaul.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://raw.githubusercontent.com/ariyaan-0/ariyaan-0.github.io/fea6cdc9b7192f0c1e3bfba88910d4637ac36c43/public/resume_rezaulHossain.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: {
     languages: ['Python', 'JavaScript', 'C++'],
